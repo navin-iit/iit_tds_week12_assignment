f655afe2e332c48530c9f20f4f58ffe610a4837a
unknown <21f1003002@student.onlinedegree.iitm.ac.in>
2022-11-28 10:28:21 +0530

    fix: Division by zero

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ def main():
         submit_button = st.form_submit_button(label='Divide')
 
     if submit_button:
-        result = float(dividend)/float(divisor)
-        st.success( "{} divided by {} = {}".format(dividend, divisor, result))
+        if float(divisor) != 0:
+            result = round(float(dividend)/float(divisor),2)
+            st.success( "{} &#247; {} = {}".format(dividend, divisor, result))
+        else:
+            st.error("Divisor should NOT be zero (0)")    
 
 
 if __name__ == '__main__':
