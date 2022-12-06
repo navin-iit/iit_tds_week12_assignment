55a1b26c76fbc7641396c9ef20b0d75a541c42d8
unknown <21f1003002@student.onlinedegree.iitm.ac.in>
2022-12-06 12:50:41 +0530

    fix: Restored Env

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ def main():
         submit_button = st.form_submit_button(label='Divide')
 
     if submit_button:
-        if float(divisor) != 0:
-            result = round(float(dividend)/float(divisor),2)
-            st.success( "{} &#247; {} = {}".format(dividend, divisor, result))
-        else:
-            st.error("Divisor should NOT be zero (0)")    
-
+        try:
+            if float(divisor) != 0:
+                result = round(float(dividend)/float(divisor),4)
+                st.success( "{} &#247; {} = {}".format(dividend, divisor, result))
+            else:
+                st.error("Divisor should NOT be zero (0)")    
+        except:        
+            st.error("Dividend and Divisor must be numbers")
 
 if __name__ == '__main__':
     main()
